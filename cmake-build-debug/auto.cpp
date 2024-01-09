54cab704f805b5f913f622acfd45dc86fd5d3841
RicnyF33 <98098844+RicnyF33@users.noreply.github.com>
2024-01-09 08:25:24 +0100

    Add files via upload

---
+++ cmake-build-debug/auto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Standard on 08.01.2024.
+//
+
+#include "auto.h"
---
+++ cmake-build-debug/auto.h
@@ -0,0 +1,16 @@
+//
+// Created by Standard on 08.01.2024.
+//
+
+#ifndef AUTO_AUTO_H
+#define AUTO_AUTO_H
+
+
+class
+
+auto {
+
+};
+
+
+#endif //AUTO_AUTO_H
